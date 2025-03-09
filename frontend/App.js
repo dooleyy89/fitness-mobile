82e6ff599439ad7f1000d2de760ea0aabad57886
david dooley <ddooley5@emich.edu>
2025-03-09 14:50:20 -0400

    Adjusted screen background color to fit device theme (light/dark)

--- frontend/App.js
+++ frontend/App.js
@@ -1,28 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import {Appearance} from 'react-native'; // to detect if device is using light/dark mode
 
 // View -> ios:UIView   android:androidview
 export default function App() {
+  const colorScheme = Appearance.getColorScheme();
+
+  const styles = StyleSheet.create({
+    title: {
+      color: colorScheme === 'dark' ? '#fff' : '#000', // based on app preference
+      fontWeight: "bold",
+    },
+    baseText: {
+      color: colorScheme === 'dark' ? '#fff' : '#000', // based on app preference
+    },
+    container: {
+      flex: 1,
+      backgroundColor: colorScheme === 'dark' ? '#222' : '#fff', // based on app preference
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
   
-  console.log('hi lol'); 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.baseText}>Open up App.js to start working on your app</Text>
+      <Text style={styles.baseText} id='title'>FitApp</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  baseText: {
-    color: "white",
-    fontWeight: "bold",
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#0a0f30',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
- 
